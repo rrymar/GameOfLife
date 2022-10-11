18e89f9d8a8bc8c18e8c0b92bf7b9df00caee540
RuslanRymar <rrimar@idiq.com>
2022-10-11 11:23:29 +0300

    init webpack

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+﻿import * as _ from 'lodash-es';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
