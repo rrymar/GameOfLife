807385ae883a1b1514152b9610d6919c41ceb12e
RuslanRymar <rrimar@idiq.com>
2022-10-11 12:03:31 +0300

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 ﻿const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/index.ts',
@@ -9,7 +10,7 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-            },
+            }
         ],
     },
     resolve: {
@@ -22,5 +23,21 @@ module.exports = {
     optimization: {
         usedExports: true,
     },
-    mode: "development"
+    mode: "development",
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "public", to: "" },
+            ],
+        }),
+    ],
+    
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 8080,
+    }
 };
